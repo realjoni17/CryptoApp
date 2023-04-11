a64044f5c2d9f8e136a4472b0f36ad1c0998c66d
realjoni17 <jaunivashisth@gmail.com>
2023-04-12 02:05:23 +0530

    Initial commit

---
+++ app/src/main/java/com/android/cryptoapp/ui/theme/Theme.kt
@@ -0,0 +1,40 @@
+package com.android.cryptoapp.ui.theme
+
+import android.graphics.Color
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+
+
+private val DarkColorPalette = darkColors(
+    primary = ColorPrimary,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray
+)
+
+private val LightColorPalette = lightColors(
+    primary = ColorPrimary,
+    background = androidx.compose.ui.graphics.Color.White,
+    onBackground = MediumGray,
+    onPrimary = DarkGray
+)
+
+@Composable
+fun CryptocurrencyAppYTTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
